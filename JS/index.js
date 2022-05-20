5093e3f2b1b0b9669bc4dcbc800fc75eceafcfeb
roberts6 <roberts_oscar@hotmail.com>
2022-05-20 14:02:22 -0500

    agrego condicionales y ciclo for

--- JS/index.js
+++ JS/index.js
@@ -6,21 +6,34 @@ alert("Te invito a comer milanesa de " + milanesa + " con " + acompanamiento + "
 
 let amigos = prompt("Con cuántos/as amigos/as te vas a juntar el finde?")
 let cocas = prompt("Cuántos/as toman fernet?")
-alert("Si son " + amigos + " vas a tener que comprar " + cocas * 2.4 + " cocas porque se van a quedar cortos/as sino")
+let cantidadCocas = cocas * 2
+alert("Si son " + amigos + " vas a tener que comprar " + cantidadCocas + " cocas porque se van a quedar cortos/as sino")
 
 
 //multiplicación
 let anos = prompt("Cuántos años tenés?")
 let numeroFav = prompt("Tu número favorito del 1 al 10")
-let pesos = (anos * numeroFav) / 5
+let pesos = (anos * numeroFav) / 4
 alert("Hoy te vas a encontrar $ " + pesos )
 
 //imprimir en el HTML // se usan comillas simples invertidas
 let imprimirEnPantalla = document.getElementById("imprimirEnHTML");
 imprimirEnPantalla.innerHTML= `
 <h1> Hoy vienen ${amigos} amigos a tu casa </h1>
-<h3> pero por favor no te olvides las ${cocas} cocas. Sino la milanesa de ${milanesa} te queda atorada</h3>
+<h3> pero por favor no te olvides las ${cantidadCocas} cocas. Sino la milanesa de ${milanesa} te queda atorada</h3>
 `;
+
+//condicional --> el + antes del = concatena con el mensaje anterior
+if (amigos >= 6){
+    imprimirEnPantalla.innerHTML += `uufff ` + amigos + ` ...Son muchos. Linda juntada`;
+}else {
+    imprimirEnPantalla.innerHTML += `Algo íntimo, me gusta`;
+}
+
+// Ciclo
+for(let i = 1; i <= amigos; i++ ){
+    imprimirEnPantalla.innerHTML += ` el amigo` + ` ` + i + ` `;
+}
 
 
 
