bdc936a0c72360501fe0c79e1fb58ab1136bfc93
roberts6 <roberts_oscar@hotmail.com>
2022-05-19 22:45:27 -0500

    primer ejercicios de JS

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+//prompt + alert
+let milanesa = prompt("Milanesa de pollo o carne?")
+let acompanamiento = prompt("con pure o con ensalada?")
+alert("Te invito a comer milanesa de " + milanesa + " con " + acompanamiento + ". Pero la cerveza la pagás vos")
+
+
+let amigos = prompt("Con cuántos/as amigos/as te vas a juntar el finde?")
+let cocas = prompt("Cuántos/as toman fernet?")
+alert("Si son " + amigos + " vas a tener que comprar " + cocas * 2 + " cocas porque se van a quedar cortos/as sino")
+
+
+
+//torneos ganados
+let copasInternacionales = 22
+let copasNacionales = 15 
+let torneosLocales = 34
+
+
+let torneosBoca = copasInternacionales + copasNacionales + torneosLocales
+
+console.log ("Boca tiene " + torneosBoca + " trofeos entre copas y torneos")
+
+//multiplicación
+
+
+
