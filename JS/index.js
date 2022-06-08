ea5a82a5017b06e7da6967deb37d822a24799858
roberts6 <roberts_oscar@hotmail.com>
2022-06-07 22:45:30 -0500

    subida de class con function adentro

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,9 @@
-//prompt + alert
+/*  //prompt + alert
 let milanesa = prompt("Milanesa de pollo o carne?")
 let acompanamiento = prompt("con pure o con ensalada?")
-/*alert("Te invito a comer milanesa de " + milanesa + " con " + acompanamiento + ". Pero la cerveza la pagás vos")*/
+alert("Te invito a comer milanesa de " + milanesa + " con " + acompanamiento + ". Pero la cerveza la pagás vos")*/
 
-switch (milanesa) {
+/* switch (milanesa) {
     case "carne":
             alert("Te invito a comer milanesa de " + milanesa)
         break;
@@ -28,8 +28,10 @@ let numeroFav = prompt("Tu número favorito del 1 al 10")
 let pesos = (anos * numeroFav) / 3
 alert("Hoy te vas a encontrar $ " + pesos )
 
-//imprimir en el HTML // se usan comillas simples invertidas
+//imprimir en el HTML // se usan comillas simples invertidas */
 let imprimirEnPantalla = document.getElementById("imprimirEnHTML");
+
+/*
 imprimirEnPantalla.innerHTML= `
 <h1> Hoy vienen ${amigos} amigos a tu casa y ${cocas} van a tomar Fernet </h1>
 <h3> Así que por favor no te olvides las ${cantidadCocas} cocas. Sino las milanesas de ${milanesa} les quedan atoradas</h3>
@@ -47,36 +49,8 @@ for(let i = 1; i <= amigos; i++ ){
     imprimirEnPantalla.innerHTML += ` El amigo` + ` ` + i + ` `;
 }
 
-// while
-/*let estadoCivil = prompt("Cómo es tu estado sentimental?")
-while (estadoCivil !== "casado" || "casada") {
-    switch (estadoCivil) {
-        case "soltero":
-            imprimirEnPantalla.innerHTML += `divertite querido`
-            break;
-            case "soltera":
-            imprimirEnPantalla.innerHTML += `divertite querida`
-            break;
-        default:
-                imprimirEnPantalla.innerHTML += `andate a tu casa pica flor`
-            break;
-    }
-    alert("ojo...")
-}
-*/
 
-
-
-/*while (amigos) {
-    if (amigos % 2 == 0) {
-        imprimirEnPantalla.innerHTML += ` Si son ` + amigos + ` da para jugar al truco de a 3`
-        break;
-    } else{
-        imprimirEnPantalla.innerHTML += ` Si son ` + amigos + ` les falta 1 para el truco`
-        break;
-    }
-}*/
-
+//switch
 switch (amigos % 2) {
     case 0:
         imprimirEnPantalla.innerHTML += ` <h2> Si son ` + amigos + ` da para jugar al truco en parejas. Pueden armar ` + (amigos)/2 +  ` equipos. </h2>`
@@ -104,4 +78,48 @@ let torneosLocales = 34
 
 let torneosBoca = copasInternacionales + copasNacionales + torneosLocales
 
-console.log ("Boca tiene " + torneosBoca + " trofeos entre copas y torneos")
+console.log ("Boca tiene " + torneosBoca + " trofeos entre copas y torneos") */
+
+
+/* let plan = prompt ("El plan médico que necesitás es individual o es familiar?")
+
+const familia = prompt("Cuántos son en la familia? (incluyéndote)")
+
+const individual = prompt ("")
+
+class cliente {
+ constructor(nombreCompleto, edad){
+ this.nombreCompleto = nombreCompleto;
+ this.edad = edad;
+}
+}
+
+switch (plan) {
+    case individual:
+        imprimirEnPantalla.innerHTML += ` <h2> El plan que mejor se adapta a tus necesidades tiene un valor de $500 final. </h2>` 
+        break;
+
+    case familiar:
+        imprimirEnPantalla.innerHTML += ` <h2> Si son ` + familia + ` sería mejor este plan. El valor es de $ ` + (familia)*405,6 +  ` total. </h2>`
+        break;
+
+    default:
+            imprimirEnPantalla.innerHTML += `<h2> No existe el plan que estás buscando <h2>`
+        break;
+} */
+
+let nombre = prompt ("Hola! ¿cuál es tu nombre?")
+let edad = prompt (nombre + " ¿este año cuántos cumpliste? si aun no fue tu cumpleaños cuántos vas a cumplir?")
+
+class categoria {
+    constructor (nombre, edad) {
+this.nombre = nombre;
+this.edad = edad;
+this.anoNacimiento = 2022 - this.edad;
+    }
+}
+
+const categoriaNueva = new categoria(nombre, edad);
+console.log(categoriaNueva);
+console.log("Naciste en el año " + categoriaNueva.anoNacimiento);
+console.log("Gracias por participar " + categoriaNueva.nombre);
