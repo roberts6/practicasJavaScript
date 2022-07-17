2bea12dd7071cc0bf3ec8d40f4c2722bac4f6bb4
roberts6 <roberts_oscar@hotmail.com>
2022-07-17 12:55:59 -0500

    queda todo seteado para entrega final

--- JS/pruebas.js
+++ JS/pruebas.js
@@ -2,24 +2,39 @@ const fechaHoy = new Date();
 
 // clase que construye socio con su información
 class Socio {
-    constructor (nombre,apellido, edad) {
+    constructor (nombre,apellido, fechaNac) {
 this.nombre = nombre;
 this.apellido = apellido;
-this.edad = edad;
-//this.dia = dia;
-//this.mes = mes;
-//this.anoNacimiento = anoNacimiento;
-this.estado = (edad > 10) ? "Mayor" : "Menor" // --> operador ternario. Evalua una condición y retorna, en este caso, 2 posibles respuestas
+this.fechaNac = fechaNac;
+this.edad = this.calcularEdad();
+
+this.estado = (this.edad > 10) ? "Mayor" : "Menor" // --> operador ternario. Evalua una condición y retorna, en este caso, 2 posibles respuestas
 this.numeroDeSocio = Math.round (Math.random() * 10000);;
 //this.anoActual = ano.getFullYear();
-    }
-    anoDeNacimiento(){
-       //return this.anoActual - this.edad;
-       //return this.diaYhora() 
     }
     SocioID (){
         return this.numeroDeSocio * 2;
     }
+    calcularEdad(){
+      const fechaActual = new Date();
+      const anoActual = parseInt(fechaActual.getFullYear());
+      const mesActual = parseInt(fechaActual.getMonth());
+      const diaActual = parseInt(fechaActual.getDay());
+  
+      const anoNacimientoSocio = parseInt(String(this.fechaNac).substring(0,4));
+      const mesNacimientoSocio = parseInt(String(this.fechaNac).substring(5,7));
+      const diaNacimientoSocio = parseInt(String(this.fechaNac).substring(8,10)); 
+
+      let edadActual = anoActual - anoNacimientoSocio
+      if (mesActual < mesNacimientoSocio) {
+        edadActual--;
+      }else if (mesActual === mesNacimientoSocio) {
+        if (diaActual < diaNacimientoSocio) {
+          edadActual--;
+        }
+      }
+      return edadActual;
+     }
 }
 
 
@@ -58,6 +73,7 @@ class Socios{
             const Nsocio = socio.numeroDeSocio
             const nombre = socio.nombre 
             const apellido = socio.apellido 
+            const edad = socio.edad
 
             // crea un div por cada elemento
             const elememto = document.createElement("div")
@@ -67,12 +83,13 @@ class Socios{
             elememto.innerHTML = `
             <div class= "nombreSocio">Nombre: ${nombre}</div>
             <div class= "nombreSocio">Apellido: ${apellido}</div>
-            <div class= "edadSocio">Edad: ${socio.edad}</div>
-            <div class= "edadSocio">Año de nacimiento: ${socio.anoDeNacimiento()}</div>
-            <div class= "edadSocio">Nº de socio: ${socio.numeroDeSocio}</div>
+            <div class= "edadSocio">Edad: ${edad}</div>
+            <div class= "edadSocio">Abono: ${socio.estado}</div>
+            <div class= "edadSocio">Año de nacimiento: ${socio.fechaNac}</div>
+            <div class= "edadSocio">Nº de socio: ${Nsocio}</div>
             `  
 
-            // creo botón para borrar
+        // creo botón para borrar
         const botonBorrar = document.createElement("button")
         botonBorrar.textContent = "Borrar"
         // acción del botón
@@ -134,7 +151,8 @@ class Socios{
             <div class= "nombreSocio">Nombre: ${socio?.nombre}</div>
             <div class= "nombreSocio">Apellido: ${socio?.apellido}</div>
             <div class= "edadSocio">Edad: ${socio?.edad}</div>
-            <div class= "edadSocio">Año de nacimiento: ${socio?.anoDeNacimiento()}</div>
+            <div class= "edadSocio">Abono: ${socio?.estado}</div>
+            <div class= "edadSocio">Año de nacimiento: ${socio?.fechaNac}</div>
             <div class= "edadSocio">Nº de socio: ${socio?.numeroDeSocio}</div>
             `
             const botonBorrar = document.createElement("button")
@@ -190,15 +208,6 @@ class Socios{
     }
     creaJson(){
     localStorage.setItem("listadoSocios", JSON.stringify(this.listaNuevosSocios))
-    //fetch('/storage.json',{
-    //  method: 'POST',
-    //  body: JSON.stringify(this.listaNuevosSocios),
-    //  Headers: {
-    //    'content-type': 'application/json; charset=UTF-8'
-    //  }
-    //})
-    //.then((response) => response.json())
-    //.then((data) => console.log("a ver qué muestra esto",data));
     }
     muestraJson(){
        let storage = localStorage.getItem("listadoSocios")
@@ -207,20 +216,67 @@ class Socios{
             console.log("En el localStorage hay guardados:",JSON.parse(storage));
         } 
     }
-    fetchStorage(){
+    menoresEdad(socio){
+        const menores = this.listaNuevosSocios.filter(socio => socio.edad <= 10)
+        console.log("Estos son los socios menores de edad:",menores)
+    const menoresEdad = [];
+  for (let index = 0; index < menores.length; index++) {
+        menoresEdad.push(menores[index].nombre)
+    }
+    let listaMenores = menoresEdad.toString(" ");
+}
+    mayoresEdad(socio){
+        const mayores = this.listaNuevosSocios.filter(socio => socio.edad > 10)
+    console.log("Estos son los socios mayores de edad:",mayores)
+
+    const mayoresEdad = [];
+    for (let index = 0; index < mayores.length; index++) {
+        mayoresEdad.push(mayores[index].nombre)   
+    }
+    let listaMayores = mayoresEdad.toString(" ");
+    }
+    diaYhora(){
+      let contenedorFecha = document.getElementById("fecha")
+        const elememtoFecha = document.createElement("div")
+        elememtoFecha.className = "horaYfecha"
+        const DateTime = luxon.DateTime
+      const intervalo = setInterval(() => { // --> este intervalo hace que cada 1'' se ejecute la const now, resfrescado el reloj
+       const now = DateTime.now();
+        elememtoFecha.innerHTML = `
+            ${now.day} / ${now.month} / ${now.year}
+            ${now.hour}:${now.minute}
+            `
+            contenedorFecha.append(elememtoFecha)
+          }, 1000);
+        } 
+    fetchStorage(socio){
       fetch('/storage.json').then((response) => response.json())
       .then((resultado) => {
         console.log("Esto trae el JSON ",resultado);
+
+        let nombre = resultado.nombre
+        let apellido = resultado.apellido
+        let fechaNac = resultado.fechaNac
+        let socio = new Socio (nombre,apellido,parseInt(fechaNac))
+        
+        this.agregarSocio(socio)
+        this.creaJson()
+        this.mayoresEdad(socio)
+        this.menoresEdad(socio)
+        this.muestraJson()
+
+        console.log("ahora así está el array", this.listaNuevosSocios);
         let contenedor = document.getElementById("contenedor")
         const elememto = document.createElement("div")
-        elememto.id = resultado?.numeroDeSocio
+        elememto.id = socio?.numeroDeSocio
         elememto.className = "socio"
         elememto.innerHTML = `
-        <div class= "nombreSocio">Nombre: ${resultado?.nombre}</div>
-        <div class= "nombreSocio">Apellido: ${resultado?.apellido}</div>
-        <div class= "edadSocio">Edad: ${resultado?.edad}</div>
-        <div class= "edadSocio">Edad: ${resultado?.numeroDeSocio}</div>
-        <div class= "edadSocio">Nº de socio: ${resultado?.numeroDeSocio}</div>
+        <div class= "nombreSocio">Nombre: ${socio?.nombre}</div>
+        <div class= "nombreSocio">Apellido: ${socio?.apellido}</div>
+        <div class= "edadSocio">Edad: ${socio?.edad}</div>
+        <div class= "edadSocio">Abono: ${socio?.estado}</div>
+        <div class= "edadSocio">Año de nacimiento: ${socio?.fechaNac}</div>
+        <div class= "edadSocio">Nº de socio: ${socio?.numeroDeSocio}</div>
         `
         const botonBorrar = document.createElement("button")
         botonBorrar.textContent = "Borrar"
@@ -269,73 +325,93 @@ class Socios{
                 }
               })
         }
-     
-        elememto.append(botonBorrar)
-        contenedor.append(elememto)
-      }).catch((error) => {console.log(error)
-    })
-}
-  fetchAPI(){
-    fetch('https://swapi.dev/api/people').then((response) => response.json())
-      .then((resultado) => {
-        console.log("Esto trae el JSON del API ",resultado);
-        for (let index = 0; index < resultado.length; index++) {
-          const persona = array[index];
+         
+            elememto.append(botonBorrar)
+            contenedor.append(elememto)
+        }).catch((error) => {console.log(error)
+        })
+    }
+    fetchAPI()
+    {
+      fetch('https://swapi.dev/api/people').then((response) => response.json())
+        .then((resultado) => {
+          console.log("Esto trae el JSON del API ",resultado.results[0].name);
           
+          for (let i = 0; i < resultado.results.length; i++) {
+      
+        let nombre = resultado.results[i].name
+        let apellido = resultado.results[i].skin_color
+        let fechaNac = "1987/12/26"
+        let socio = new Socio (nombre,apellido,parseInt(fechaNac))
+        
+        SOCIOS.agregarSocio(socio)
+        
           let contenedor = document.getElementById("contenedor")
-        const elememto = document.createElement("div")
-        elememto.id = persona[index]?.numeroDeSocio
-        elememto.className = "socio"
-        elememto.innerHTML = `
-        <div class= "nombreSocio">Nombre: ${persona[index]?.nombre}</div>
-        <div class= "nombreSocio">Apellido: ${persona[index]?.apellido}</div>
-        <div class= "edadSocio">Edad: ${persona[index]?.edad}</div>
-        <div class= "edadSocio">Edad: ${persona[index]?.numeroDeSocio}</div>
-        <div class= "edadSocio">Nº de socio: ${persona[index]?.numeroDeSocio}</div>
-        `
-        const botonBorrar = document.createElement("button")
-        botonBorrar.textContent = "Borrar"
-      }
-      }).catch((error) => {console.log(error)
-    })
-  }
-    menoresEdad(socio){
-        const menores = this.listaNuevosSocios.filter(socio => socio.edad <= 10)
-        console.log("Estos son los socios menores de edad:",menores)
-    const menoresEdad = [];
-  for (let index = 0; index < menores.length; index++) {
-        menoresEdad.push(menores[index].nombre)
+          const elememto = document.createElement("div")
+          elememto.id = socio?.numeroDeSocio
+          elememto.className = "socio"
+          elememto.innerHTML = `
+          <div class= "nombreSocio">Nombre: ${socio?.nombre}</div>
+          <div class= "nombreSocio">Apellido: ${socio?.apellido}</div>
+          <div class= "edadSocio">Edad: ${socio?.edad}</div>
+          <div class= "edadSocio">Abono: ${socio?.estado}</div>
+          <div class= "edadSocio">Año de nacimiento: ${socio?.fechaNac}</div>
+          <div class= "edadSocio">Nº de socio: ${socio?.numeroDeSocio}</div>
+          `
+          const botonBorrar = document.createElement("button")
+          botonBorrar.textContent = "Borrar"
+          botonBorrar.onclick = () => {
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                  confirmButton: 'btn btn-success',
+                  cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+              })
+              
+              swalWithBootstrapButtons.fire({
+                title: '¿Estás seguro que querés eliminar a ' + socio.nombre + '?',
+                text: "Esta acción no se puede revertir!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Si, eliminar!',
+                cancelButtonText: 'No, cancelar!',
+                reverseButtons: true
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  swalWithBootstrapButtons.fire(
+                    'HECHO!',
+                    'El socio ha sido eliminado.',
+                    'success'
+                  )
+                  this.borrarSocio(socio)
+                  this.borrarSocioHTML(socio)
+                  localStorage.removeItem(socio)
+                  this.LogArray()
+                  this.menoresEdad(socio)
+                  this.mayoresEdad(socio)
+                  this.creaJson()
+                  this.muestraJson()
+                } else if (
+                  /* Read more about handling dismissals below */
+                  result.dismiss === Swal.DismissReason.cancel
+                ) {
+                  swalWithBootstrapButtons.fire(
+                    'Cancelado',
+                    'No ha habido cambios',
+                    'error'
+                  )
+                }
+              })
+        }
+         
+            elememto.append(botonBorrar)
+            contenedor.append(elememto)
+        }
+        console.log("ahora así está el array", this.listaNuevosSocios);
+        }).catch((error) => {console.log(error)
+      })
     }
-    let listaMenores = menoresEdad.toString(" ");
-}
-    mayoresEdad(socio){
-        const mayores = this.listaNuevosSocios.filter(socio => socio.edad > 10)
-    console.log("Estos son los socios mayores de edad:",mayores)
-
-    const mayoresEdad = [];
-    for (let index = 0; index < mayores.length; index++) {
-        mayoresEdad.push(mayores[index].nombre)   
-    }
-    let listaMayores = mayoresEdad.toString(" ");
-    }
-    diaYhora(){
-        const DateTime = luxon.DateTime
-       const now = DateTime.now();
-       const dt = DateTime.fromObject (
-         {now},
-         {zone: 'America/buenos-aires', numberingSystem: 'beng'}
-       )
-       const diaParaResta = (now.year,now.month,now.day)
-       console.log("esto trae Luxon ",diaParaResta);
-        let contenedorFecha = document.getElementById("fecha")
-        const elememtoFecha = document.createElement("div")
-        elememtoFecha.className = "horaYfecha"
-        elememtoFecha.innerHTML = `
-            ${now.day} / ${now.month} / ${now.year}
-            ${now.hour}:${now.minute}
-            `
-            contenedorFecha.append(elememtoFecha)
-   } 
 }
 
 
@@ -344,10 +420,10 @@ const SOCIOS = new Socios()
 
 
 // Ingreso de socios de forma estática
-const socioEstatico1 = new Socio("Beni","Roberts", 2,parseInt("2020/6/5"));
-const socioEstatico2 = new Socio("Oscar","Roberts", 34);  
-const socioEstatico3 = new Socio("Cecilia","Olguin", 33);  
-const socioEstatico4 = new Socio("Mila","Roberts", 3);
+const socioEstatico1 = new Socio("Beni","Roberts",parseInt("2020/6/5"));
+const socioEstatico2 = new Socio("Oscar","Roberts", parseInt("1987/12/26"));  
+const socioEstatico3 = new Socio("Cecilia","Olguin", parseInt("1988/4/8"));  
+const socioEstatico4 = new Socio("Mila","Roberts", parseInt("2019/2/3"));
 
 
 SOCIOS.agregarSocio(socioEstatico1)
@@ -366,22 +442,56 @@ SOCIOS.muestraJson()
 SOCIOS.diaYhora()
 SOCIOS.fetchStorage()
 SOCIOS.fetchAPI()
+//SOCIOS.fetchAPI()
+//SOCIOS.muestraApi()
 
+const inputs = document.querySelectorAll(`#formulario input`)
+let expresiones = /^[a-zA-ZÀ-ÿ\s]{1,40}$/i;
+
+const validarForm = (e) => {
+  switch (e.target.value) {
+   case "nombre":
+      if (expresiones.test(e.target.value)) {
+        document.getElementById("validationDefaultNombre").classList.remove("incorrecto")
+      }else{
+          document.getElementById("validationDefaultNombre").classList.add("incorrecto")
+      }
+      break;
+     case "apellido":
+      if (expresiones.test(e.target.value)) {
+        document.getElementById("validationDefaultNombre").classList.remove("incorrecto")
+      }else{
+        document.getElementById("validationDefaultNombre").classList.add("incorrecto")
+      }
+     break;
+  }
+}
+
+inputs.forEach((input) => {
+  console.log(input.addEventListener('keyup', validarForm));
+   
+  input.addEventListener('blur', validarForm)
+  });
 
 // toma datos del formulario y construye un socio nuevo
-const submitFormulario = (ID) => {
-    let form = document.getElementById(ID);
+const submitFormulario = async (ID) => {
+  try {
+    let form = await document.getElementById(ID);
     form.addEventListener(`submit`, (event) => {
     event.preventDefault();
     let nombre = document.querySelector("#validationDefaultNombre").value
     let apellido = document.querySelector("#validationDefaultApellido").value
-    let edad = document.querySelector("#validationDefaultFechaNac").value
-    console.log("esto trae: ",edad);
+    let fechaNac = document.querySelector("#validationDefaultFechaNac").value
     
-    let dia = document.querySelector("#validationDefaultNombre").value
-    let mes = document.querySelector("#validationDefaultNombre").value
-    let anoNacimiento = document.querySelector("#validationDefaultNombre").value
-    let socio = new Socio (nombre,apellido,parseInt(edad))
+    console.log("nació en: ",fechaNac);
+    
+
+
+    //let dia = document.querySelector("#validationDefaultNombre").value
+    //let mes = document.querySelector("#validationDefaultNombre").value
+    //let anoNacimiento = document.querySelector("#validationDefaultNombre").value
+
+    let socio = new Socio (nombre,apellido,parseInt(fechaNac))
     SOCIOS.agregarSocio(socio)
     console.log(socio);
     SOCIOS.mostrarUltimoSocio(socio)
@@ -390,7 +500,11 @@ const submitFormulario = (ID) => {
     SOCIOS.LogArray()
     SOCIOS.creaJson()
     SOCIOS.muestraJson()
-})}
+})
+  } catch (error) {
+    console.log("Error en el submit: ",error); 
+  }
+    }
 
 // invocación para que el formulario tome los datos ingresados
 submitFormulario("formulario")
